2605172d33676d5a88bb6e25e174a5cb5af8071a
punphatai <92926627+punphatai@users.noreply.github.com>
2022-02-12 01:47:55 +0700

    add file P8.JPG to img floder.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import Photo from './component/Photo';
 import img360 from "./img/360.jpeg";
+import img_P8 from "./img/P8.JPG";
 
 function App() {
   return (
     <div className="container-fluid bg-dark text-light p-5 text-center">
       <h1>hello</h1>
-      <Photo src={img360} />
+      <Photo src={img_P8} />
     </div>
   );
 }
