932c75450bbcb06a7d188c6732af05d0e55be4c0
nithan-git <82077874+nithan-git@users.noreply.github.com>
2022-02-12 13:38:02 +0700

    fix import Photosphere

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import Photo from './component/Photo';
+import Photosphere from './component/Photosphere';
 import img_P8 from "./img/P8.JPG";
 
 function App() {
   return (
     <div className="container-fluid bg-dark text-light p-5 text-center">
       <h1>hello</h1>
-      <Photo src={img_P8} />
+      <Photosphere src={img_P8} />
     </div>
   );
 }
