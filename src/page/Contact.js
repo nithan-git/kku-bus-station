4f97be4cd93f9a3396dcefb32d8e3578bfad4f5c
nithan-git <82077874+nithan-git@users.noreply.github.com>
2022-02-16 06:29:12 +0700

    add contact infommation

--- src/page/Contact.js
+++ src/page/Contact.js
@@ -1,7 +1,19 @@
+import images from "../img/Images.js";
 
 function Contact() {
   return (
-    <h1>Contact Page</h1>
+    <div className="container-fluid bg-dark text-light p-5 text-center">
+      <h1>MaskedRiderDouble</h1>
+      <div className="container ">
+        <img src={images["panithan.jpg"]}  className="rounded my-3" alt="Panithan" width="30%"></img>      
+        <p>ชื่อ ปณิธาน เทพบุดดี รหัสนักศึกษา 623020477-5 <br/>สาขาวิทยาการคอมพิวเตอร์ วิทยาลัยคอมพิวเตอร์ มหาวิทยาลัยขอนแก่น</p>
+      </div>
+      <div className="container">
+        <img src={images["panithan.jpg"]}  className="rounded my-3" alt="Panithan" width="30%"></img>      
+        <p>ชื่อ ปณิธาน เทพบุดดี รหัสนักศึกษา 623020477-5 <br/>สาขาวิทยาการคอมพิวเตอร์ วิทยาลัยคอมพิวเตอร์ มหาวิทยาลัยขอนแก่น</p>
+      </div>
+      
+    </div>
   );
 }
 
