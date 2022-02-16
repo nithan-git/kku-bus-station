d185c17fe861999c6f6e0ad5672c14eb4f03bc95
punphatai <92926627+punphatai@users.noreply.github.com>
2022-02-16 22:57:39 +0700

    change css style  contact page.

--- src/page/Contact.js
+++ src/page/Contact.js
@@ -2,31 +2,38 @@ import images from "../img/Images.js";
 
 function Contact() {
   return (
-    <div className="container-fluid bg-dark text-light p-5 text-center">
-      <h1 className="my-3">MaskedRiderDouble</h1>
-      <div className="container p-5">
+    <div className="container-fluid bg-dark text-light p-5">
+      <h1 className="my-3 text-center">MaskedRiderDouble</h1>
+      <div className="profile container p-5">
         <img
           src={images["panithan.jpg"]}
           className="rounded my-3"
           alt="Panithan"
           width="30%"
         ></img>
-        <p>
-          ชื่อ ปณิธาน เทพบุดดี รหัสนักศึกษา 623020477-5 <br />
-          สาขาวิทยาการคอมพิวเตอร์ วิทยาลัยคอมพิวเตอร์ มหาวิทยาลัยขอนแก่น
-        </p>
+        <div class='detail'>
+          <p>ชื่อ: ปณิธาน เทพบุดดี</p>
+          <p>รหัสนักศึกษา: 623020477-5</p>
+          <p>สาขา: วิทยาการคอมพิวเตอร์ </p>
+          <p>คณะ: วิทยาลัยการคอมพิวเตอร์</p>
+          <p>มหาวิทยาลัยขอนแก่น</p>
+        </div>
       </div>
-      <div className="container p-5">
+      <hr></hr>
+      <div className="profile container p-5">
         <img
           src={images["pun.jpg"]}
           className="rounded my-3"
           alt="Panithan"
           width="30%"
         ></img>
-        <p>
-          ชื่อ นายปัณณธร ผาใต้ รหัสนักศึกษา 623020745-6 <br />
-          สาขาวิทยาการคอมพิวเตอร์ วิทยาลัยคอมพิวเตอร์ มหาวิทยาลัยขอนแก่น
-        </p>
+        <div class='detail'>
+          <p>ชื่อ: ปัณณธร ผาใต้</p>
+          <p>รหัสนักศึกษา: 623020745-6</p>
+          <p>สาขา: วิทยาการคอมพิวเตอร์ </p>
+          <p>คณะ: วิทยาลัยการคอมพิวเตอร์</p>
+          <p>มหาวิทยาลัยขอนแก่น</p>
+        </div>
       </div>
     </div>
   );
