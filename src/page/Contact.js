2933a65f3e8e0ac4afb7b9df2ac36d615f76eb69
nithan-git <82077874+nithan-git@users.noreply.github.com>
2022-02-16 10:34:40 +0700

    edit contact infomation second to pun

--- src/page/Contact.js
+++ src/page/Contact.js
@@ -4,15 +4,40 @@ function Contact() {
   return (
     <div className="container-fluid bg-dark text-light p-5 text-center">
       <h1>MaskedRiderDouble</h1>
-      <div className="container ">
-        <img src={images["panithan.jpg"]}  className="rounded my-3" alt="Panithan" width="30%"></img>      
-        <p>ชื่อ ปณิธาน เทพบุดดี รหัสนักศึกษา 623020477-5 <br/>สาขาวิทยาการคอมพิวเตอร์ วิทยาลัยคอมพิวเตอร์ มหาวิทยาลัยขอนแก่น</p>
+      <div className="container-sm">
+        <div className="row">
+          <div className="col">
+            <img
+              src={images["panithan.jpg"]}
+              className="img-fluid rounded my-3"
+              alt="Panithan"
+              width="30%"
+            ></img>
+          </div>
+          <div className="col">
+            <img
+              src={images["pun.jpg"]}
+              className="img-fluid rounded my-3"
+              alt="Panithan"
+              width="30%"
+            ></img>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col">
+            <p>
+              ชื่อ ปณิธาน เทพบุดดี รหัสนักศึกษา 623020477-5 <br />
+              สาขาวิทยาการคอมพิวเตอร์ วิทยาลัยคอมพิวเตอร์ มหาวิทยาลัยขอนแก่น
+            </p>
+          </div>
+          <div className="col">
+            <p>
+              ชื่อ นายปัณณธร ผาใต้ รหัสนักศึกษา 623020745-6 <br />
+              สาขาวิทยาการคอมพิวเตอร์ วิทยาลัยคอมพิวเตอร์ มหาวิทยาลัยขอนแก่น
+            </p>
+          </div>
+        </div>
       </div>
-      <div className="container">
-        <img src={images["panithan.jpg"]}  className="rounded my-3" alt="Panithan" width="30%"></img>      
-        <p>ชื่อ ปณิธาน เทพบุดดี รหัสนักศึกษา 623020477-5 <br/>สาขาวิทยาการคอมพิวเตอร์ วิทยาลัยคอมพิวเตอร์ มหาวิทยาลัยขอนแก่น</p>
-      </div>
-      
     </div>
   );
 }
