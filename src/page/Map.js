51c07dc0daa5348d857333ba70b0bd3838a65638
punphatai <92926627+punphatai@users.noreply.github.com>
2022-02-20 18:56:15 +0700

    fix scroll-box that over flow.

--- src/page/Map.js
+++ src/page/Map.js
@@ -2,7 +2,16 @@ import images from "../img/Images.js";
 
 function Map() {
   return (
-    <div className="container-fluid text-center bg-dark">
+    <div className="text-center bg-dark">
+      <div className="main-box bg-white text-center">
+        <div className="scroll-box">
+          <img className="bus-img img-fluid m-5" src={images['kku-bus.png']} alt="kku-bus"></img>
+          <div className="main-det">
+            <p>kku bus station</p>
+            <p>สายสีม่วง</p>
+          </div>
+        </div>
+      </div>
       <img className="img-fluid m-5" src={images['map.jpg']} alt="map" style={{width: "1000px",height:"100%"}}></img>
     </div>
     
