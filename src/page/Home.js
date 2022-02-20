c4af98f27ee8acb144c8eb9b66b2b27e16c96289
punphatai <92926627+punphatai@users.noreply.github.com>
2022-02-20 14:57:40 +0700

    change logo, add new image to floder and add scroll move to page Home.

--- src/page/Home.js
+++ src/page/Home.js
@@ -1,9 +1,23 @@
 import BusStationPhotosphere from "../component/BusStationPhotosphere";
+import images from "../img/Images.js";
 
 function Home() {
   return (
-    <div className="container-fluid text-light p-5 text-center" style={{width:"80%"}}>
-      <BusStationPhotosphere/>
+    <div className="bg-dark">
+      <div className="main-box bg-white text-center">
+        <div className="scroll-box">
+          <img className="img-fluid m-5" src={images['kku-bus.png']} alt="kku-bus"></img>
+          <div className="main-det">
+            <p>kku bus station</p>
+            <p>สายสีม่วง</p>
+          </div>
+        </div>
+      </div>
+      <div className="panorama-pic-box" style={{width:"100%",height:"100%"}}>
+        <div className="container-fluid text-light p-5 text-center" style={{width:"80%"}}>
+          <BusStationPhotosphere/>
+        </div>
+      </div>
     </div>
   );
 }
--- src/page/Map.js
+++ src/page/Map.js
@@ -2,8 +2,8 @@ import images from "../img/Images.js";
 
 function Map() {
   return (
-    <div className="container-fluid text-center">
-      <img className="img-fluid m-5" src={images['map.jpg']} alt="map"></img>
+    <div className="container-fluid text-center bg-dark">
+      <img className="img-fluid m-5" src={images['map.jpg']} alt="map" style={{width: "1000px",height:"100%"}}></img>
     </div>
     
   );
